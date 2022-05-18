5226fd463a220099a7d8bcb765eb3b191621b46f
Jonas Serafim <serajonas@gmail.com>
2022-05-18 00:24:16 +0000

    aula 1

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask("Hello")
+
+@app.route("/hello")
+def hello():
+    return render_template("hello.html")
+
+@app.route("/tchau")
+def tchau():
+    return "Tchau !!!"
